fccb20964eb3c81c00e6befd298cdf6c844e09e0
Stan <stan@Stans-MacBook-Pro.local>
2021-08-15 19:54:15 -0700

    finished tutorial vid 4

--- hackershack_website/apps/accounts/urls.py
+++ hackershack_website/apps/accounts/urls.py
@@ -4,12 +4,12 @@ from . import views
 
 app_name="accounts"
 urlpatterns = [
-    path("accounts/profile", views.ProfileView.as_view(), name="profile"),
+    path("profile", views.ProfileView.as_view(), name="profile"),
     # Django Auth
     path(
-        "accounts/login",
+        "login",
         auth_views.LoginView.as_view(template_name="accounts/login.html"),
         name="login",
     ),
-    path("accounts/logout", auth_views.LogoutView.as_view(), name="logout"),
+    path("logout", auth_views.LogoutView.as_view(), name="logout"),
 ]
